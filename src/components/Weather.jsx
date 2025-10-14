7d75fdddb743728fe4616da102e25476eb3ce616
Utkarsh Ragghuvanshi <utkarshragghuvanshi@Utkarshs-MacBook-Air.local>
2025-10-14 17:21:05 +0530

    state for entering location dynamically

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -6,11 +6,14 @@ export default function Weather() {
   // 1. using open weather map api
   // 1.1 Purpose to this state to store the weather data fetched from Open Weather Map API
   const [data, setData] = useState({});
+  // 2 Creating state to manage dynamic location
+  // User type the location, will be stored in location
+  const [location, setLocation] = useState("");
   // 1.2 Serach function -> resposible of fetching weather data from API
   const search = async () => {
     try {
       const API_KEY = "0857bdfbf9822bcb5f4d0f481d5e160a";
-      const URL = `https://api.openweathermap.org/data/2.5/weather?q=Varanasi&units=Metric&appid=${API_KEY}`;
+      const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=${API_KEY}`;
 
       // 1.3 http get request using axios
       const response = await axios.get(URL);
@@ -18,10 +21,22 @@ export default function Weather() {
 
       // 1.4 update the data/state after fetching the response
       setData(response.data);
+      // 2.4 set user location to '' once user already serched the location
+      // this will help user to enter new location
+      // Enchance UI by providing immediate Visual feedback and search was succesfull
+
+      setLocation("");
       console.log("data", data);
     } catch (error) {
       console.error("Error fetching weather data:", error);
     }
+  };
+
+  // 2.2 function to update the location whatever user types
+  // This function will be called every time user types in the input field
+  const handleInputChange = (evt) => {
+    evt.preventDefault();
+    setLocation(evt.target.value);
   };
 
   return (
@@ -32,7 +47,14 @@ export default function Weather() {
           <div className="location">Banglore </div>
         </div>
         <div className="search-location">
-          <input type="text" placeholder="Enter Location" />
+          <input
+            type="text"
+            placeholder="Enter Location"
+            // 2.3 set value for input entered and handle event change
+            value={location}
+            // Input field value will be controlled by react state
+            onChange={handleInputChange}
+          />
           <i className="fa-solid fa-magnifying-glass"></i>
         </div>
         <div className="weather-data">

fb49b25f591b8d594008df3261d91d7b7f543fcf
Utkarsh Ragghuvanshi <utkarshragghuvanshi@Utkarshs-MacBook-Air.local>
2025-10-27 19:37:58 +0530

    Deployment 2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/news-and-blogs-react/",
+  base: "/",
   define: {
     "process.env": {}, // This can fix basic 'process is not defined' errors
   },
